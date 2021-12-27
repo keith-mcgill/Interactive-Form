01366e25979db2367992168944232611f06fea5d
keloholic <72310838+keloholic@users.noreply.github.com>
2021-12-27 12:45:33 -0600

    added Job Role section functionality

--- js/script.js
+++ js/script.js
@@ -1,4 +1,22 @@
 //Accesses the name field in the Dom
 const nameField = document.getElementById('name');
+//accesses the other-job-role field in the DOM
+const otherJobRoleField = document.getElementById('other-job-role');
+//accesses the job role dropdown DOM element
+const jobRoleDropdown = document.getElementById('title');
+
+
 //assigns the name input focus state
 nameField.focus();
+
+//hides the other job role field upon page load
+otherJobRoleField.setAttribute('type', 'hidden');
+
+//Hides or displays the other job role field based on user selection
+jobRoleDropdown.addEventListener('change', (e) => {
+    if(e.target.value === 'other'){
+        otherJobRoleField.setAttribute('type', 'text')
+    } else {
+        otherJobRoleField.setAttribute('type', 'hidden')
+    }
+});
